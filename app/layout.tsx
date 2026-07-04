29136446224b14997b920b117027f8e610d8ea3a
Ankit meena <ankitmeenapc1@gmail.com>
2026-07-04 18:32:11 +0530

    complete todoApp

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,12 +1,16 @@
 import './globals.css'  // ← 'globals.css' (plural 's')
 import ThemeProvider from '@/components/ThemeProvider'
+import { ReactNode } from "react";
 
 export const metadata = {
   title: 'Todo App',
   description: 'A simple todo app with dark mode',
 }
+type RootLayoutProps = {
+  children: ReactNode;
+};
 
-export default function RootLayout({ children }) {
+export default function RootLayout({ children }: RootLayoutProps) {
   return (
     <html lang="en" suppressHydrationWarning>
       <body>
